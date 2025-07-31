89123bd7ebc9ed22d55de950ee4bdc735bc24824
RESRAS SILEN <mugafelhida98@gmail.com>
2025-07-31 13:26:25 -0700

    Updated bg in main layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <CartProvider>
-        <body className="relative">
+        <body className="bg-white relative">
           <RouteLoader />
           <ChatWhatsapp />
           {children}
