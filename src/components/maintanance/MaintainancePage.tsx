7162e1276a714ea7579e8dbddbab76bfffefb48e
RESRAS SILEN <mugafelhida98@gmail.com>
2025-07-31 15:42:17 -0700

    fixed build issues

--- src/components/maintanance/MaintainancePage.tsx
+++ src/components/maintanance/MaintainancePage.tsx
@@ -4,7 +4,7 @@ export default function MaintenancePage() {
       <div className="flex items-center justify-center h-screen bg-gray-100 text-center">
         <div>
           <h1 className="text-3xl font-bold mb-4">🚧 Site Under Maintenance</h1>
-          <p className="text-lg">We're working hard to finish the development. Please check back soon.</p>
+          <p className="text-lg">We&apos;re working hard to finish the development. Please check back soon.</p>
         </div>
       </div>
     );
