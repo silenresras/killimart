ae60b346f6453e9777323210f7059ef40096f39a
RESRAS SILEN <mugafelhida98@gmail.com>
2025-08-01 08:32:40 -0700

    Updated the the font in maintainance page

--- src/components/maintanance/MaintainancePage.tsx
+++ src/components/maintanance/MaintainancePage.tsx
@@ -3,8 +3,8 @@ export default function MaintenancePage() {
     return (
       <div className="flex items-center justify-center h-screen bg-gray-100 text-center">
         <div>
-          <h1 className="text-3xl font-bold mb-4">🚧 Site Under Maintenance</h1>
-          <p className="text-lg">We&apos;re working hard to finish the development. Please check back soon.</p>
+          <h1 className="text-3xl font-bold mb-4 text-black">🚧 Site Under Maintenance</h1>
+          <p className="text-lg text-black">We&apos;re working hard to finish the development. Please check back soon.</p>
         </div>
       </div>
     );
