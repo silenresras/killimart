9fd1275b6356a424975958ecd9192455c3f478eb
RESRAS SILEN <mugafelhida98@gmail.com>
2025-08-08 12:24:51 -0700

    Updated the maintanance page

--- src/components/maintanance/MaintainancePage.tsx
+++ src/components/maintanance/MaintainancePage.tsx
@@ -3,8 +3,7 @@ export default function MaintenancePage() {
     return (
       <div className="flex items-center justify-center h-screen bg-gray-100 text-center">
         <div>
-          <h1 className="text-3xl font-bold mb-4 text-black">🚧 Site Under Maintenance</h1>
-          <p className="text-lg text-black">We&apos;re working hard to finish the development. Please check back soon.</p>
+          <h1 className="text-3xl font-bold mb-4 text-black"> FIND ANYTHING ELSE TO DO MOTHERFUCKER 🖕 🖕 🖕</h1>
         </div>
       </div>
     );
