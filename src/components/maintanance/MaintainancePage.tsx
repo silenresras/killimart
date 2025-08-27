dedb98ea5d0504296ea6a7f696c01a2dc70f2907
RESRAS SILEN <mugafelhida98@gmail.com>
2025-08-26 23:52:58 -0700

    Repaired to fix build errors

--- src/components/maintanance/MaintainancePage.tsx
+++ src/components/maintanance/MaintainancePage.tsx
@@ -3,7 +3,7 @@ export default function MaintenancePage() {
     return (
       <div className="flex items-center justify-center h-screen bg-gray-100 text-center">
         <div>
-          <h1 className="text-3xl font-bold mb-4 text-black">WE'RE IN DEVELOPMENT NEW THINGS COMING IN YOUR WAY. THANKS!!!</h1>
+          <h1 className="text-3xl font-bold mb-4 text-black"> IN DEVELOPMENT NEW THINGS COMING IN YOUR WAY. THANKS!!!</h1>
         </div>
       </div>
     );
