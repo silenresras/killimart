bc59f7437e2600d3f26c1e5d9edd4d9db96f02d0
RESRAS SILEN <mugafelhida98@gmail.com>
2025-12-30 10:17:13 -0800

    Updated NextJs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**', // allow all paths from this host
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
-}
+};
 
 module.exports = nextConfig;
